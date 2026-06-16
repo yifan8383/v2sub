dd64ccb039f2595d3639c708cdc6e94b37dee222
runoneall <runoobsteve@gmail.com>
2026-06-16 16:00:08 +0800

    feat: enhance SourceEditor component layout with form elements for better user input

--- web/src/components/SourceEditor.vue
+++ web/src/components/SourceEditor.vue
@@ -25,7 +25,23 @@ const handleClose = done => {
 </script>
 
 <template>
-    <el-dialog :model-value="prop.show" title="源编辑器" width="90%" :before-close="handleClose" align-center></el-dialog>
+    <el-dialog :model-value="prop.show" title="源编辑器" width="90%" :before-close="handleClose" align-center>
+        <el-form :model="prop.data" label-width="auto" style="max-width: 600px">
+            <el-form-item label="名称">
+                <el-input v-model="prop.data.name" :disabled="prop.data.name !== ''" />
+            </el-form-item>
+            <el-form-item label="地址">
+                <el-input v-model="prop.data.url" />
+            </el-form-item>
+            <el-form-item label="排除">
+                <el-input v-model="prop.data.exclude" placeholder="以 ',' 分隔" />
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" :loading="prop.loading" @click="emit('save')">保存</el-button>
+                <el-button :loading="prop.loading" @click="emit('close')">取消</el-button>
+            </el-form-item>
+        </el-form>
+    </el-dialog>
 </template>
 
 <style scoped></style>
